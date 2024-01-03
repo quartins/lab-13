01a1344f1383a8bea060c45d4b7f7f1e1d6a09b7
nani <chotima.nani@gmail.com>
2024-01-03 13:58:09 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,29 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double keep[],int n,double output[]){
+    double arithemtic = 0 ,stand = 0,geometric = 1 ,harmonic = 0,max=keep[0] ,min = keep[0];
+    for( int i = 0; i < n ; i++)
+    {
+        arithemtic += keep[i];
+        geometric *= keep[i];
+        harmonic += 1/keep[i];
+        if(keep[i]>max) max = keep[i];
+        else if(keep[i]<min) min = keep[i];
+    }
+    
+    output[0] = arithemtic/n;
+
+    for( int i = 0; i < n ; i++)
+    {
+        stand += pow(keep[i] - output[0], 2);
+    }
+
+    output[1] = sqrt(stand/n);
+    output[2] = pow(geometric,1.0/n);
+    output[3] = n/harmonic;
+    output[4] = max ;
+    output[5] = min ;
+
+}
